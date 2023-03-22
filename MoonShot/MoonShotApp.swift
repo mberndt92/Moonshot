9f876f3e196c7630b60c6a3a16045285f88da07c
mberndt92 <maximilian_berndt@icloud.com>
2023-03-22 20:05:03 +0900

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Maximilian Berndt on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
